8c31bf5401bff343485c95898410306fba4698ab
Orlando Yurivilca <sergioyv@gmail.com>
2022-01-19 16:49:08 -0500

    logica funcionando

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,26 @@
 puts "Cleaning database..."
 Restaurant.destroy_all
 
+Review.destroy_all
+
+
+
 puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone: '111-222-333', category: 'belgian' }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone: '444-222-333', category: 'french'  }
-tanta =  { name: "tanta", address: "miraflores", phone: '9899-222-333', category: 'japanese'  }
-mama_mia =  { name: "Mama mia", address: "italia", phone: '555-222-333', category: 'italian'  }
-ni_hao =  { name: "Ni Hao", address: "china", phone: '888-222-333', category: 'chinese'  }
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: '111-222-333', category: 'belgian' }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: '444-222-333', category: 'french'  }
+tanta =  { name: "tanta", address: "miraflores", phone_number: '9899-222-333', category: 'japanese'  }
+mama_mia =  { name: "Mama mia", address: "italia", phone_number: '555-222-333', category: 'italian'  }
+ni_hao =  { name: "Ni Hao", address: "china", phone_number: '888-222-333', category: 'chinese'  }
 
 [ dishoom, pizza_east, tanta, mama_mia, ni_hao ].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
 end
+
+puts "creating 1 review"
+review_1 = { rating: 4, content: "delicious", restaurant_id: 1 }
+review = Review.create!(review_1)
+
 puts "Finished!"
+
+# 
